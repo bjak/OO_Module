1106d71bb5f088da6382858d444e53e4035d1746
bjak <blake_jakobsson@utexas.edu>
2018-07-30 13:36:20 -0500

    Reviseed SeeNSay

--- SeeNSay.py
+++ SeeNSay.py
@@ -2,46 +2,29 @@
 
 class SeeNSay:
     """SeeNSay Class"""
-    def spin(self, animal, animal_noise):
-        """Spinning will print the corresponding animal name with its sound"""
+    def __init__(self, animal, animal_noise):
+        """Init Instances"""
+        
         self.animal = animal 
         self.animal_noise = animal_noise
-        return "The %s says %s" % (self.animal, self.animal_noise) #prints the animal with the correct animal noise
+        
+    def spin(self):
+        """Spin will return Aniaml with Noise"""
+        return "The %s says %s" % (animal, animal_noise) #prints the animal with the correct animal noise
+
+Pig = ('Pig', 'Oink')
+Cat = ('Cat', 'Meow')
+Cow = ('Cow', 'Moo')
 
 class Animal:
     """Animal class"""
-    def animal_noise(self, noise):
+    def animal_noise(self):
         """Animal Noise Method"""
-        self.noise = noise
         pass
-    def animal_name(self, name):
+    def animal_name(self):
         """Animal Name Method"""
-        self.name = name
         pass
 
-class Pig(Animal):
-    """Pig Subclass"""
-    def pig_name(self):
-        """Pig Name"""
-        return self.name = "Pig"
-    def pig_noise(self):
-        """Pig Noise"""
-        return self.noise = "Oink"
-
-class Cow(Animal):
-    """Cow Subclass"""
-    def cow_name(self):
-        """Cow Name"""
-        return self.name = "Cow"
-    def cow_noise(self):
-        """Cow Noise"""
-        return self.noise = "Moo"
-
-class Cat(Animal):
-    """Cat Subclass"""
-    def cat_name(self):
-        """Cat Name"""
-        return self.name = "Cat"
-    def cat_noise(self):
-        """Cat Noise"""
-        return self.noise = "Meow"
+print(SeeNSay.spin(Pig))
+print(SeeNSay.spin(Cat))
+print(SeeNSay.spin(Cow))
