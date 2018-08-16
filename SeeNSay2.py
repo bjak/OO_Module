6354d37553edc1c562d1ee4ca6e69d01ef129ea0
bjak <blake_jakobsson@utexas.edu>
2018-08-16 11:16:33 -0500

    Updates to SeeNSay2

--- SeeNSay2.py
+++ SeeNSay2.py
@@ -5,53 +5,58 @@ import random
 import string
 
 class SeeNSay:
+    """SeeNSay Class"""
 
     def __init__(self):
+        """Initialize Function"""
 
         pass
 
-    def animals(self,animal, animal_noise):
+    def animals(self, animal, animal_noise):
+        """Animals Definition"""
 
         self.animal = animal
         self.animal_noise = animal_noise
 
     def spin(self):
-
-        ##print("The %s says %s!") % (self.name, self.noise)
-        names = ['Pig', 'Dog', 'Cow']
-        name = random.choice(names)
-        #"".join(choice(animal))
-        sounds = ['Oink', 'Ruff', 'Moo']
-        sound = random.choice(sounds)
-        #"".join(choice(sound))
-        print("The " + name + " " + "says " + sound)
+        """Spin Function"""
+        # print("The %s says %s!") % (self.name, self.noise)
+        # names = ['Pig', 'Dog', 'Cow']
+        # name = random.choice(names)
+        # "".join(choice(name))
+        # sounds = ['Oink', 'Ruff', 'Moo']
+        # sound = random.choice(sounds)
+        # "".join(choice(sound))
+        print("The " + self.name + " " + "says " + self.noise)
 
 class Animal:
-
+    """Animal Class"""
     def name(self, name):
-
+        """Name Function"""
+        self.name = name
         return self.name
 
     def noise(self, noise):
-
+        """Noise Function"""
+        self.noise = noise
         return self.noise
 
 class Pig(Animal):
-
+    """Pig Class"""
     Pig = SeeNSay()
     Pig.name = "Pig"
     Pig.noise = "Oink"
     Pig.spin()
 
 class Dog(Animal):
-
+    """Dog Class"""
     Dog = SeeNSay()
     Dog.name = "Dog"
     Dog.noise = "Ruff"
     Dog.spin()
 
 class Cow(Animal):
-
+    """Cow Class"""
     Cow = SeeNSay()
     Cow.name = "Cow"
     Cow.noise = "Moo"
